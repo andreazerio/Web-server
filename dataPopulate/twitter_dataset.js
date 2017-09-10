4b8abe431fdd2f4be46d57591aa08f285c9bcf11
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-09-10 17:26:20 +0100

    refactored code splitting twitter data populate from index

---
+++ dataPopulate/twitter_dataset.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+require('dotenv').config({path: './twitter_keys.env' });
+var Twit = require('twit');
+const request_options = {
+    hostname: 'northwitter-api-wqhhzdeecj.now.sh',
+    path: '/handles',
+    method: 'GET',
+};
+const currentPath = process.cwd();
+var T = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
+});
+
+//Creating dataset with Twitter data
+
+const twitterData = https.request(request_options, function(response) {
+    response.on('data', function(dataset) {
+        fs.writeFile(`${currentPath}/handles_name.json`,dataset, function (err) {
+            if(err) throw err;
+        })
+        JSON.parse(dataset).handles.forEach(function(screen_name) { 
+            T.get('statuses/user_timeline', {screen_name: screen_name, count : 50}, function(err, data, response) {
+                            fs.writeFile(`${currentPath}/data/tweets/${handle}.json`, JSON.stringify(data), function(err) {
+                                if(err) throw err;
+                            }); 
+                     
+                });
+            T.get('users/show',{screen_name: screen_name}, function(err, data, response) {
+                fs.writeFile(`${currentPath}/data/users/${handle}.json`, JSON.stringify(data), function(err) {
+                    if(err) throw err;
+                }); 
+        })
+    });
+});
+
+twitterData.on('error', function(error) {
+    console.log(error);
+});
+});
+twitterData.end();
--- index.js
+++ index.js
@@ -1,58 +1,24 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-require('dotenv').config({path: './twitter_keys.env' });
-var Twit = require('twit');
-const request_options = {
-    hostname: 'northwitter-api-wqhhzdeecj.now.sh',
-    path: '/handles',
-    method: 'GET',
-};
 const currentPath = process.cwd();
-var T = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret
-});
-
-//Creating dataset with Twitter data
-
-const twitterData = https.request(request_options, function(response) {
-    response.on('data', function(dataset) {
-        fs.writeFile(`${currentPath}/handles_name.json`,dataset, function (err) {
-            if(err) throw err;
-        })
-        JSON.parse(dataset).handles.forEach(function(handle) { 
-            T.get('statuses/user_timeline', {screen_name: handle, count : 50}, function(err, data, response) {
-                            fs.writeFile(`${currentPath}/data/tweets/${handle}.json`, JSON.stringify(data), function(err) {
-                                if(err) throw err;
-                            }); 
-                     
-                });
-            T.get('users/show',{screen_name: handle}, function(err, data, response) {
-                fs.writeFile(`${currentPath}/data/users/${handle}.json`, JSON.stringify(data), function(err) {
-                    if(err) throw err;
-                }); 
-        })
-    });
-});
-
-twitterData.on('error', function(error) {
-    console.log(error);
-});
-});
-twitterData.end();
-
 function control_panel (request,response) {
 
-    if (request.method === 'GET' && request.url === '/') {
+    let incomingRequest = `${request.method} ${response.url}`
+
+    if (incomingRequest = 'GET /') {
        let interface = fs.readFileSync(`${currentPath}/interface/home.html`);
        response.statusCode = 200;
        response.setHeader('Content-Type', 'text, html');
        response.write(interface);
        response.end();
     }
+
+    else if( incomingRequest === 'GET /handles') {
+
+
+    }
+
     else {
         response.write('Invalid path');
     }
